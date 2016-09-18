ba68544d2b65c6820667a46a09062b53de8bc2f1
bharatsn123 <bharatsn123@gmail.com>
2016-09-18 11:23:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/bharu', function (req,res)
-{res.send(heya)}
+{res.send('heya!!')}
 );
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
